278d3d5fff1ec29c7ae8d57dde43e78b8ebcc260
ning <ningyuan.sg@gmail.com>
2018-07-13 12:23:42 +0800

    Change graderProgram -> graderPrograms, an array

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ EOF
 resource "aws_lambda_function" "test_lambda" {
   filename      = "grader.zip"
   function_name = "grader"
-  handler       = "index.myHandler"
+  handler       = "index.runAll"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   runtime       = "nodejs8.10"
   source_code_hash = "${base64sha256(file("grader.zip"))}"
