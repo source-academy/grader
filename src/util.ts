3285d0008f48fc7841daedc6d1551ad8d4d53837
Yuchuan <yyc478@gmail.com>
2018-08-28 13:12:07 +0800

    type fixes

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,13 @@
+var table = {};
+// to stop typescript complaining
+const globalAny:any = global;
+
 /**
   Utility function that wraps a function and provides two tracking functions
   to allow you to track whether a function has been called
   Type: (fn) -> fn
 **/
-var table = {};
-const track_function = (name: String) => {
+const track_function = (name: string) => {
   if (table[name] !== undefined) {
     reset_count(name);
     return;
@@ -18,14 +21,14 @@ const track_function = (name: String) => {
   global[name] = wrapped_function;
 };
 
-const reset_count = (name: String) => {
+const reset_count = (name: string) => {
   table[name] = 0;
 };
 
-const get_count = (name: String) => {
+const get_count = (name: string) => {
   return table[name];
 };
 
-global.__track_function = track_function;
-global.__reset_function_count = reset_count;
-global.__get_function_count = get_count;
+globalAny.__track_function = track_function;
+globalAny.__reset_function_count = reset_count;
+globalAny.__get_function_count = get_count;
