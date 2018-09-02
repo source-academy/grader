534145f99983663a247efe2cb772c7c6df2e5426
Yuchuan <yyc478@gmail.com>
2018-09-02 16:07:24 +0800

    add documentation for tracking functions

--- src/util.ts
+++ src/util.ts
@@ -6,6 +6,22 @@ const globalAny:any = global;
   Utility function that wraps a function and provides two tracking functions
   to allow you to track whether a function has been called
   Type: (fn) -> fn
+
+  To use in a grader function, remember to include this in the globals so the tracking function gets overridden before the values are bound. For instance, to track make_point, add:
+
+  <EXTERNAL name="CURVES">
+
+      <SYMBOL>make_point</SYMBOL>
+      ...
+      <SYMBOL>__reset_function_count</SYMBOL>
+      <SYMBOL>__get_function_count</SYMBOL>
+  </EXTERNAL>
+  <GLOBAL>
+    <IDENTIFIER>__tracking_function_initializer</IDENTIFIER>
+    <VALUE>global.__track_function("make_point");</VALUE>
+  </GLOBAL>
+
+  And you will be able to use __reset_function_count and __get_function_count in your functions.
 **/
 const track_function = (name: string) => {
   if (table[name] !== undefined) {
