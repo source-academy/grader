ab6fbb970c0b97afb7ba385077a4f03d48837487
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-10-11 01:41:31 +0800

    Replace stream with the correct file

--- src/streams/streams.js
+++ src/streams/streams.js
@@ -1,292 +1,335 @@
-//Reverse engineering streams.js from https://github.com/dionyziz/stream.js/blob/master/lib/index.js
+// stream.js: Supporting streams in the Scheme style, following
+//            "stream discipline"
+// A stream is either the empty list or a pair whose tail is
+// a nullary function that returns a stream.
+
+// Author: Martin Henz
+
+// stream_tail returns the second component of the given pair
+// throws an exception if the argument is not a pair
 // LOW-LEVEL FUNCTION, NOT JEDISCRIPT
-function apply(f, xs) {
-    var args = [];
-    var len = length(xs);
-    for (var i = 0; i < len; i++) {
-        args[i] = head(xs);
-        xs = tail(xs);
+function stream_tail(xs) {
+    var tail;
+    if (is_pair(xs)) {
+        tail = xs[1];
+    } else {
+        throw new Error("stream_tail(xs) expects a pair as "
+            + "argument xs, but encountered " + xs);
     }
-    return f.apply(f, args);
+
+    if (typeof tail === "function") {
+        return tail();
+    } else {
+        throw new Error("stream_tail(xs) expects a function as "
+            + "the tail of the argument pair xs, "
+            + "but encountered " + tail);
+    }
 }
 
+// is_stream recurses down the stream and checks that it ends with
+// the empty list []; does not throw any exceptions
 // LOW-LEVEL FUNCTION, NOT JEDISCRIPT
-var array_to_list = function(arr) {
-    var res = [];
-    for (var i = arr.length - 1; i >= 0; i--) res = pair(arr[i], res);
-    return res;
-};
-
-var the_empty_stream = [];
-var stream_head = head;
-
-//Fufills a promise and then memoizes the value of that execution.
-//LOW-LEVEL FUNCTION, NOT JEDISCRIPT
-function force(promise) {
-    var res = promise();
-    promise = function() {
-        return res;
-    };
-    return res;
+// Lazy? No: is_stream needs to go down the stream
+function is_stream(xs) {
+    return (array_test(xs) && xs.length === 0)
+    || (is_pair(xs) && typeof tail(xs) === "function" &&
+            is_stream(stream_tail(xs)));
 }
 
-//
-// LOW-LEVEL FUNCTION, NOT JEDISCRIPT
-function stream_tail(stream) {
-    if (!is_pair(stream) || typeof tail(stream) != 'function')
-        throw new Error(
-                'stream_tail(stream) expects [element, function()] given ' + stream,
-                );
-    return force(tail(stream));
-}
-
-var stream_null = is_empty_list;
-var is_empty_stream = stream_null;
-
-//stream usage tools
-
-//prints the first n terms of the stream to console.
-function show_stream(stream, nterms) {
-    console.log(eval_stream(stream, nterms));
-}
-
-//converts the first n terms of a stream into a list.
-function eval_stream(stream, n) {
-    var res = [];
-    var tstream = stream;
-    while (!is_empty_list(tstream) && n > 0) {
-        res = pair(head(tstream), res);
-        tstream = stream_tail(tstream);
-        n--;
-    }
-    return reverse(res);
-}
-
-//returns a finite stream consisting of the first n terms of a stream.
-function stream_take(str, n) {
-    if (n == 0) return the_empty_stream;
-    else
-        return pair(head(str), function() {
-                return stream_take(stream_tail(str), n - 1);
-                });
-}
-
-//returns a stream after discarding the first n elements of it.
-function stream_drop(
-        str,
-        n, //renamed from stream_tail
-        ) {
-    if (n == 0) return str;
-    else return stream_drop(stream_tail(str), n - 1);
-}
-
-//returns the element in the stream at position pos
-function stream_ref(str, pos) {
-    return head(stream_drop(str, pos));
-}
-
-//returns a infinite stream of k, k, k ....
-function stream_constant(k) {
-    return pair(k, function() {
-            return stream_constant(k);
-            });
-}
-
-//applies proc to each element of the stream
-//returns said result.
-function stream_map(proc) {
-    arguments.slice = [].slice; //hacks!
-    var argstreams = array_to_list(arguments.slice(1, arguments.length)); //emulation!
-    if (is_empty_list(head(argstreams))) return the_empty_stream;
-    else
-        return pair(apply(proc, map(head, argstreams)), function() {
-                return apply(stream_map, pair(proc, map(stream_tail, argstreams)));
-                });
-}
-
-//adds the corresponding elements of each stream.
-function add_streams() {
-    arguments.slice = [].slice; //hacks!
-    //console.log(arguments);
-    var argstreams = array_to_list(arguments.slice(0, arguments.length)); //emulation!
-    function plus() {
-        var res = 0;
-        for (var i = 0; i < arguments.length; i++) res += arguments[i];
-        return res;
-    }
-    return apply(stream_map, pair(plus, argstreams));
-}
-
-//multiplies the corresponding elements of each stream.
-function mul_streams() {
-    arguments.slice = [].slice; //hacks!
-    var argstreams = array_to_list(arguments.slice(0, arguments.length)); //emulation!
-    function mul() {
-        var res = 1;
-        for (var i = 0; i < arguments.length; i++) res *= arguments[i];
-        return res;
-    }
-    return apply(stream_map, pair(mul, argstreams));
-}
-
-//multiplies each element in the stream by k.
-function scale_stream(k, stream) {
-    return mul_streams(stream_constant(k), stream);
-}
-
-var integers = pair(1, function() {
-        return add_streams(stream_constant(1), integers);
-        });
-var non_neg_integers = pair(0, function() {
-        return integers;
-        });
-
-//appends s2 to s1.
-function stream_append(s1, s2) {
-    if (stream_null(s1)) return s2;
-    else
-        return pair(head(s1), function() {
-                return stream_append(stream_tail(s1), s2);
-                });
-}
-
-/*(define (stream-pairs s)
-  (if (stream-null? s)
-  the-empty-stream
-  (stream-append
-  (stream-map
-  (lambda (sn) 
-  (list (stream-car s) sn))
-  (stream-cdr s))
-  (delay (stream-pairs (stream-cdr s))))))*/
-
-//generates all pairs for a given finite stream. original implementation is bugged.
-function stream_pairs(s) {
-    if (stream_null(s)) return the_empty_stream;
-    else
-        return stream_append(
-                stream_map(function(sn) {
-                    return list(head(s), sn);
-                    }, stream_tail(s)),
-                pairs(stream_tail(s)),
-                );
-}
-
-//returns a stream with only elements satisfying pred.
-function stream_filter(pred, stream) {
-    if (stream_null(stream)) return the_empty_stream;
-    else if (pred(head(stream)))
-        return pair(head(stream), function() {
-                return stream_filter(pred, stream_tail(stream));
-                });
-    else return stream_filter(pred, stream_tail(stream));
-}
-
-//replaces all occurances in the str of a with b.
-function replace_stream(str, a, b) {
-    var temp;
-    if (head(str) === a) temp = b;
-    else temp = head(str);
-    return pair(temp, function() {
-            return replace_stream(stream_tail(str), a, b);
-            });
-}
-
-//power series operations
-var add_series = add_streams;
-
-var scale_series = scale_stream;
-
-function negate_series(s) {
-    return scale_series(-1, s);
-}
-
-function subtract_series(s1, s2) {
-    return add_series(s1, negate_series(s2));
-}
-
-// create a (finite) series from a list of coefficients
-// the rest of the coefficients will naturally be 0s.
-function coeffs_to_series(list_coeffs) {
-    var zeros = pair(0, function() {
-            return zeros;
-            });
-    var iter = function(list) {
-        if (is_empty_list(list)) return zeros;
-        else
-            return pair(head(list), function() {
-                    return iter(tail(list));
+// list_to_stream transforms a given list to a stream
+// Lazy? Yes: list_to_stream goes down the list only when forced
+function list_to_stream(xs) {
+    if (is_empty_list(xs)) {
+        return [];
+    } else {
+        return pair(head(xs),
+                    function() {
+                        return list_to_stream(tail(xs));
                     });
-    };
-    return iter(list_coeffs);
-}
-
-//converts a list to a finite stream
-function list_to_stream(lst) {
-    return accumulate(
-            function(x, y) {
-            return pair(x, function() {
-                    return y;
-                    });
-            },
-            [],
-            lst,
-            );
-}
-
-//creates a sequence from a procedure such that the nth term is P(n)
-function proc_to_sequence(proc) {
-    return stream_map(proc, non_neg_integers);
-}
-
-//to transplant...
-//(proc init lst ...+)
-function foldl(
-        proc,
-        init, //from left to right
-        ) {
-    arguments.slice = [].slice; //hacks!
-    var argstreams = array_to_list(arguments.slice(2, arguments.length)); //emulation!
-    while (
-            !is_empty_list(head(argstreams)) //normally it should expect same length (i think), but mine will terminate based on first list.
-          ) {
-        init = apply(proc, append(map(head, argstreams), list(init)));
-        argstreams = map(tail, argstreams);
     }
-    return init;
 }
 
-function foldr(proc, init) {
-    arguments.slice = [].slice; //hacks!
-    var argstreams = array_to_list(arguments.slice(2, arguments.length)); //emulation!
-    argstreams = map(reverse, argstreams);
-    var test = append(list(proc, init), argstreams);
-    return apply(foldl, test);
+// stream_to_list transforms a given stream to a list
+// Lazy? No: stream_to_list needs to force the whole stream
+function stream_to_list(xs) {
+    if (is_empty_list(xs)) {
+        return [];
+    } else {
+        return pair(head(xs), stream_to_list(stream_tail(xs)));
+    }
 }
 
-global.is_empty_stream = is_empty_stream;
-global.show_stream = show_stream;
-global.eval_stream = eval_stream;
-global.stream_take = stream_take;
-global.stream_drop = stream_drop;
-global.stream_ref = stream_ref;
-global.stream_constant = stream_constant;
-global.stream_map = stream_map;
-global.add_streams = add_streams;
-global.mul_streams = mul_streams;
-global.scale_stream = scale_stream;
-global.integers = integers;
-global.non_neg_integers = non_neg_integers;
-global.stream_append = stream_append;
-global.stream_pairs = stream_pairs;
-global.stream_filter = stream_filter;
-global.replace_stream = replace_stream;
-global.add_series = add_series;
-global.scale_series = scale_series;
-global.negate_series = negate_series;
-global.subtract_series = subtract_series;
-global.coeffs_to_series = coeffs_to_series;
+// stream makes a stream out of its arguments
+// LOW-LEVEL FUNCTION, NOT JEDISCRIPT
+// Lazy? No: In this implementation, we generate first a
+//           complete list, and then a stream using list_to_stream
+function stream() {
+    var the_list = [];
+    for (var i = arguments.length - 1; i >= 0; i--) {
+        the_list = pair(arguments[i], the_list);
+    }
+    return list_to_stream(the_list);
+}
+
+// stream_length returns the length of a given argument stream
+// throws an exception if the argument is not a stream
+// Lazy? No: The function needs to explore the whole stream
+function stream_length(xs) {
+    if (is_empty_list(xs)) {
+        return 0;
+    } else {
+        return 1 + stream_length(stream_tail(xs));
+    }
+}
+
+// stream_map applies first arg f to the elements of the second
+// argument, assumed to be a stream.
+// f is applied element-by-element:
+// stream_map(f,list_to_stream([1,[2,[]]])) results in
+// the same as list_to_stream([f(1),[f(2),[]]])
+// stream_map throws an exception if the second argument is not a
+// stream, and if the second argument is a non-empty stream and the
+// first argument is not a function.
+// Lazy? Yes: The argument stream is only explored as forced by
+//            the result stream.
+function stream_map(f, s) {
+    if (is_empty_list(s)) {
+       return [];
+    } else {
+        return pair(f(head(s)),
+                    function() {
+                        return stream_map(f, stream_tail(s));
+                    });
+    }
+}
+
+// build_stream takes a non-negative integer n as first argument,
+// and a function fun as second argument.
+// build_list returns a stream of n elements, that results from
+// applying fun to the numbers from 0 to n-1.
+// Lazy? Yes: The result stream forces the applications of fun
+//            for the next element
+function build_stream(n, fun) {
+    function build(i) {
+        if (i >= n) {
+            return [];
+        } else {
+            return pair(fun(i),
+                        function() {
+                            return build(i + 1);
+                        });
+        }
+    }
+    return build(0);
+}
+
+// stream_for_each applies first arg fun to the elements of the list
+// passed as second argument. fun is applied element-by-element:
+// for_each(fun,list_to_stream([1,[2,[]]])) results in the calls fun(1)
+// and fun(2).
+// stream_for_each returns true.
+// stream_for_each throws an exception if the second argument is not a list,
+// and if the second argument is a non-empty list and the
+// first argument is not a function.
+// Lazy? No: stream_for_each forces the exploration of the entire stream
+function stream_for_each(fun, xs) {
+    if (is_empty_list(xs)) {
+        return true;
+    } else {
+        fun(head(xs));
+        return stream_for_each(fun, stream_tail(xs));
+    }
+}
+
+// stream_reverse reverses the argument stream
+// stream_reverse throws an exception if the argument is not a stream.
+// Lazy? No: stream_reverse forces the exploration of the entire stream
+function stream_reverse(xs) {
+    function rev(original, reversed) {
+        if (is_empty_list(original)) {
+            return reversed;
+        } else {
+            return rev(stream_tail(original),
+                       pair(head(original), function() { return reversed; }));
+        }
+    }
+    return rev(xs,[]);
+}
+
+// stream_to_vector returns vector that contains the elements of the argument
+// stream in the given order.
+// stream_to_vector throws an exception if the argument is not a stream
+// LOW-LEVEL FUNCTION, NOT JEDISCRIPT
+// Lazy? No: stream_to_vector forces the exploration of the entire stream
+function stream_to_vector(lst){
+    var vector = [];
+    while (!is_empty_list(lst)) {
+        vector.push(head(lst));
+        lst = stream_tail(lst);
+    }
+    return vector;
+}
+
+// stream_append appends first argument stream and second argument stream.
+// In the result, the [] at the end of the first argument stream
+// is replaced by the second argument stream
+// stream_append throws an exception if the first argument is not a
+// stream.
+// Lazy? Yes: the result stream forces the actual append operation
+function stream_append(xs, ys) {
+    if (is_empty_list(xs)) {
+        return ys;
+    } else {
+        return pair(head(xs),
+                    function() {
+                        return stream_append(stream_tail(xs), ys);
+                    });
+    }
+}
+
+// stream_member looks for a given first-argument element in a given
+// second argument stream. It returns the first postfix substream
+// that starts with the given element. It returns [] if the
+// element does not occur in the stream
+// Lazy? Sort-of: stream_member forces the stream only until the element is found.
+function stream_member(x, s) {
+    if (is_empty_list(s)) {
+        return [];
+    } else if (head(s) === x) {
+        return s;
+    } else {
+        return stream_member(x, stream_tail(s));
+    }
+}
+
+// stream_remove removes the first occurrence of a given first-argument element
+// in a given second-argument list. Returns the original list
+// if there is no occurrence.
+// Lazy? Yes: the result stream forces the construction of each next element
+function stream_remove(v, xs) {
+    if (is_empty_list(xs)) {
+        return [];
+    } else if (v === head(xs)) {
+        return stream_tail(xs);
+    } else {
+        return pair(head(xs),
+                    function() {
+                        return stream_remove(v, stream_tail(xs));
+                    });
+    }
+}
+
+// stream_remove_all removes all instances of v instead of just the first.
+// Lazy? Yes: the result stream forces the construction of each next element
+function stream_remove_all(v, xs) {
+    if (is_empty_list(xs)) {
+        return [];
+    } else if (v === head(xs)) {
+        return stream_remove_all(v, stream_tail(xs));
+    } else {
+        return pair(head(xs), function() {
+            return stream_remove_all(v, stream_tail(xs));
+        });
+    }
+}
+
+// filter returns the substream of elements of given stream s
+// for which the given predicate function p returns true.
+// Lazy? Yes: The result stream forces the construction of
+//            each next element. Of course, the construction
+//            of the next element needs to go down the stream
+//            until an element is found for which p holds.
+function stream_filter(p, s) {
+    if (is_empty_list(s)) {
+        return [];
+    } else if (p(head(s))) {
+        return pair(head(s),
+                    function() {
+                        return stream_filter(p, stream_tail(s));
+                    });
+    } else {
+        return stream_filter(p, stream_tail(s));
+    }
+}
+
+// enumerates numbers starting from start,
+// using a step size of 1, until the number
+// exceeds end.
+// Lazy? Yes: The result stream forces the construction of
+//            each next element
+function enum_stream(start, end) {
+    if (start > end) {
+        return [];
+    } else {
+        return pair(start,
+                    function() {
+                        return enum_stream(start + 1, end);
+                    });
+    }
+}
+
+// integers_from constructs an infinite stream of integers
+// starting at a given number n
+// Lazy? Yes: The result stream forces the construction of
+//            each next element
+function integers_from(n) {
+    return pair(n,
+                function() {
+                    return integers_from(n + 1);
+                });
+}
+
+// eval_stream constructs the list of the first n elements
+// of a given stream s
+// Lazy? Sort-of: eval_stream only forces the computation of
+//                the first n elements, and leaves the rest of
+//                the stream untouched.
+function eval_stream(s, n) {
+    if (n === 0) {
+        return [];
+    } else {
+        return pair(head(s),
+                    eval_stream(stream_tail(s),
+                                n - 1));
+   }
+}
+
+// Returns the item in stream s at index n (the first item is at position 0)
+// Lazy? Sort-of: stream_ref only forces the computation of
+//                the first n elements, and leaves the rest of
+//                the stream untouched.
+function stream_ref(s, n) {
+    if (n === 0) {
+        return head(s);
+    } else {
+        return stream_ref(stream_tail(s), n - 1);
+    }
+}
+
+function stream_take_max(str, n) {
+  return is_empty_list(str) || n === 0
+    ? []
+    : pair(head(str), () => stream_take_max(stream_tail(str), n - 1));
+}
+
+global.stream_tail = stream_tail;
+global.is_stream = is_stream;
 global.list_to_stream = list_to_stream;
-global.proc_to_sequence = proc_to_sequence;
-global.foldl = foldl;
-global.foldr = foldr;
+global.stream_to_list = stream_to_list;
+global.stream = stream;
+global.stream_length = stream_length;
+global.stream_map = stream_map;
+global.build_stream = build_stream;
+global.stream_for_each = stream_for_each;
+global.stream_reverse = stream_reverse;
+global.stream_to_vector = stream_to_vector;
+global.stream_append = stream_append;
+global.stream_member = stream_member;
+global.stream_remove = stream_remove;
+global.stream_remove_all = stream_remove_all;
+global.stream_filter = stream_filter;
+global.enum_stream = enum_stream;
+global.integers_from = integers_from;
+global.eval_stream = eval_stream;
+global.stream_ref = stream_ref;
+global.stream_take_max = stream_take_max;
