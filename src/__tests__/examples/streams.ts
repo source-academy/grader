3e81c3c55dbae44639f2d870ee81799b30846d85
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-10-11 10:42:43 +0800

    Add more tests

--- src/__tests__/examples/streams.ts
+++ src/__tests__/examples/streams.ts
@@ -6,12 +6,12 @@ const validStudentCorrect =
     return n % 2 === 1;
   }
 
-  const odd_stream = stream_filter(is_odd, integers_from(1));
+  const odd_stream = stream_filter(is_odd, enum_stream(1, 50));
   `
 
 const validStudentWrong =
   `
-  const odd_stream = stream_filter(n => n % 2 === 1, enum_stream(0, 1));
+  const odd_stream = stream_filter(n => n % 2 === 1, list_to_stream(list(1, 2, 3)));
   `
 
 const validStudentPartial =
@@ -20,13 +20,23 @@ const validStudentPartial =
     return n % 2 === 1;
   }
 
-  const odd_stream = stream_filter(is_odd, enum_stream(1, 5));
+  const odd_stream = stream_filter(is_odd, stream_append(enum_stream(1, 3), enum_stream(4, 5)));
+  `
+
+const invalidStudentRuntime =
+  `
+  function is_odd(n) {
+    return n % 2 === 1;
+  }
+
+  // Will stack overflow is_stream
+  const odd_stream = stream_filter(is_odd, integers_from(1));
   `
 
 export const student: Student = {
   invalid: {
-    runtime: null,
-    syntax: null
+    runtime: invalidStudentRuntime,
+    syntax: null,
   },
   valid: {
     correct: validStudentCorrect,
@@ -49,6 +59,19 @@ const validGrader = [
     return equal(lst, list(1, 3, 5, 7, 9)) ? 2 : 0;
   }
   stream_testcase_b();
+  `,
+  `
+  function stream_testcase_c() {
+    return !is_empty_list(stream_member(3, odd_stream)) &&
+      stream_ref(odd_stream, 1) === 3 ? 1 : 0;
+  }
+  stream_testcase_c();
+  `,
+  `
+  function stream_testcase_d() {
+    return is_stream(odd_stream, 1) ? 1 : 0;
+  }
+  stream_testcase_d();
   `
 ]
 
--- src/__tests__/test_streams.ts
+++ src/__tests__/test_streams.ts
@@ -11,8 +11,12 @@ const makeAwsEvent = awsEventFactory({
       'stream_to_list',
       'stream_take_max',
       'stream_tail',
+      'stream_ref',
+      'is_stream',
       'integers_from',
       'enum_stream',
+      'list_to_stream',
+      'stream_append',
       'equal',
       'list',
     ]
@@ -25,6 +29,8 @@ test('stream grader OK, student OK, correct', async () => {
   expect(results).toEqual([
     {'grade': 1, 'resultType': 'pass'},
     {'grade': 2, 'resultType': 'pass'},
+    {'grade': 1, 'resultType': 'pass'},
+    {'grade': 1, 'resultType': 'pass'},
   ])
 })
 
@@ -33,6 +39,8 @@ test('stream grader OK, student OK, wrong', async () => {
   expect(results).toEqual([
     {'grade': 0, 'resultType': 'pass'},
     {'grade': 0, 'resultType': 'pass'},
+    {'grade': 1, 'resultType': 'pass'},
+    {'grade': 1, 'resultType': 'pass'},
   ])
 })
 
@@ -41,6 +49,17 @@ test('stream grader OK, student OK, partial', async () => {
   expect(results).toEqual([
     {'grade': 1, 'resultType': 'pass'},
     {'grade': 0, 'resultType': 'pass'},
+    {'grade': 1, 'resultType': 'pass'},
+    {'grade': 1, 'resultType': 'pass'},
   ])
 })
 
+test('stream grader OK, student runtimeError', async () => {
+  const results = await runAll(makeAwsEvent(grader.valid, student.invalid.runtime))
+  expect(results).toEqual([
+    {'grade': 1, 'resultType': 'pass'},
+    {'grade': 2, 'resultType': 'pass'},
+    {'grade': 1, 'resultType': 'pass'},
+    {'errors': [{'errorType': 'runtime', 'line': 3, 'location': 'grader'}], 'resultType': 'error'},
+  ])
+})
