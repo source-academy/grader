8701a2174665eb6f0e731212fa5b65ea1d945a22
angelsl <angelsl@in04.sg>
2020-08-18 00:29:12 +0800

    Include tests in TSC but remove from built package

--- scripts/build-lambda-grader.sh
+++ scripts/build-lambda-grader.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 rm -rf build grader.zip node_modules
 JOBS=$(nproc) yarn install --frozen-lockfile
 yarn run tsc
-rm -rf node_modules
+rm -rf node_modules build/__tests__
 JOBS=$(nproc) yarn install --production --ignore-scripts --frozen-lockfile
 cp -r node_modules/ build
 
