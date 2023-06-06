2747b4fbb9e54e21ddf97038531945228c73077a
YaleChen299 <proverb299@gmail.com>
2023-06-06 13:21:32 +0800

    try new build config

--- scripts/build-lambda-grader.sh
+++ scripts/build-lambda-grader.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+yum install -y libXi-devel.x86_64 mesa-libGL-devel.x86_64 cairo-devel pango-devel pixman-devel make glibc-devel gcc patch gcc-c++
+
 rm -rf build grader.zip node_modules
 JOBS=$(nproc) yarn install --frozen-lockfile
 yarn run tsc
