8fa8091a373b78625db204ac66c6a0f9922fb691
sayomaki <sayomayomaki@gmail.com>
2026-08-20 23:48:11 +0800

    Fix formatting

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {
 } from 'js-slang/dist/createContext'
 import type { SourceError } from 'js-slang/dist/errors/base'
 import { Variant } from 'js-slang/dist/langs'
-import { Context, Frame, Value} from 'js-slang/dist/types'
+import { Context, Frame, Value } from 'js-slang/dist/types'
 import { stringify } from 'js-slang/dist/utils/stringify'
 
 import { loadCurves, loadRunes } from './graphicsLoader'
